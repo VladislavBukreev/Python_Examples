b7a557a32c5851db2faad161cbe89a4d8603d717
VladislavBukreev <bukr7@mail.ru>
2023-04-22 16:58:16 +1000

    lection3

---
+++ Лекция3/Лекция3_Модульность.py
@@ -0,0 +1,9 @@
+#import modul1 
+# from modul1 import max1 - Импортировать из modul1 функцию max1
+# inport modul1 as m1 - Переименовать для сокращения 
+from modul1 import *
+
+
+
+#print(modul1.max1(5,9))
+print(max1(5,9))
